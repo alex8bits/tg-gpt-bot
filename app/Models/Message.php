27cb6c33df2589ac1af26073b51ae6e550de63d8
Alex <jalex21@mail.ru>
2025-06-22 01:17:47 +0700

    log

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property $id
- * @property $user_id
+ * @property $customer_id
  * @property $role
  * @property $content
  */
 class Message extends Model
 {
     protected $fillable = [
-        'user_id',
+        'customer_id',
         'role',
         'content'
     ];
