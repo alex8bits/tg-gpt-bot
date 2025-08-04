8401e923f9dd8739a8ac2193f424be66d2a219da
Alex <jalex21@mail.ru>
2025-08-04 19:42:22 +0700

    force main bot

--- app/Listeners/SendDebugMessage.php
+++ app/Listeners/SendDebugMessage.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\DebugEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class SendDebugMessage
@@ -26,6 +27,8 @@ class SendDebugMessage
         if (!$message || !config('telegram.service_chat') || config('telegram.service_chat') == '') {
             return;
         }
+
+        Log::debug('sendDebugMessage', ['message' => $message]);
 
         Telegram::sendMessage([
             'chat_id' => config('telegram.service_chat'),
